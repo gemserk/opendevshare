5ec8b1de9ffc9023bf8877c50a11ed7c2540efd5
Ruben Garat <ruben.garat@gemserk.com>
2011-08-16 23:44:24 -0300

    added installed status detection, market button and uninstall button

--- opendevshare-android/src/com/gemserk/opendevshare/android/OpenDevShareMainActivity.java
+++ opendevshare-android/src/com/gemserk/opendevshare/android/OpenDevShareMainActivity.java
@@ -4,6 +4,8 @@ import java.io.File;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,30 +13,83 @@ import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class OpenDevShareMainActivity extends Activity {
-	private long enqueue;
 	private DownloadManager dm;
+	private String packageName = "com.gemserk.games.superflyingthing";
+	private Button buttonlatest;
+	private Button buttonstable;
+	private Button buttonmarket;
+	private Button buttonUninstall;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		Button buttonlatest = (Button) findViewById(R.id.button_latest);
+		buttonlatest = (Button) findViewById(R.id.button_latest);
 		buttonlatest.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
 				OpenDevShareMainActivity.this.downloadAndInstallAPK("http://www.gemserk.com/private/prototipos/superflyingthing-latest/superflyingthing-android.apk");
 			}
 		});
-		Button buttonstable = (Button) findViewById(R.id.button_stable);
+
+		buttonstable = (Button) findViewById(R.id.button_stable);
 		buttonstable.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
 				OpenDevShareMainActivity.this.downloadAndInstallAPK("http://www.gemserk.com/private/prototipos/superflyingthing-release/superflyingthing-android.apk");
 			}
 		});
+
+		buttonmarket = (Button) findViewById(R.id.button_market);
+		buttonmarket.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				Intent goToMarket = null;
+				goToMarket = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.gemserk.games.superflyingthing"));
+				startActivity(goToMarket);
+			}
+		});
+
+		buttonUninstall = (Button) findViewById(R.id.button_uninstall);
+		buttonUninstall.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				Uri uninstallUri = Uri.fromParts("package", packageName, null);
+				Intent intent = new Intent(Intent.ACTION_DELETE, uninstallUri);
+				startActivity(intent);
+			}
+		});
+
+	}
+
+	private boolean isPackageInstalled(String packageName) {
+		PackageManager pm = getPackageManager();
+		boolean app_installed = false;
+		try {
+			pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+			app_installed = true;
+		} catch (PackageManager.NameNotFoundException e) {
+			app_installed = false;
+		}
+		return app_installed;
+	}
+
+	public void checkStatusOfButtons() {
+		boolean app_installed = isPackageInstalled(packageName);
+		buttonlatest.setEnabled(!app_installed);
+		buttonstable.setEnabled(!app_installed);
+		buttonmarket.setEnabled(!app_installed);
+		buttonUninstall.setEnabled(app_installed);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		checkStatusOfButtons();
 	}
 
 	public void downloadAndInstallAPK(String url) {
@@ -43,7 +98,6 @@ public class OpenDevShareMainActivity extends Activity {
 		File externalPath = Environment.getExternalStorageDirectory();
 		File appFiles = new File(externalPath.getAbsolutePath() + "/Android/data/" + packageName + "/files");
 		appFiles.mkdirs();
-		
 
 		final String finalApkFile = new File(appFiles, "/downloadedapk.apk").getAbsolutePath();
 		DownloadManager downloadManager = new DownloadManager(this) {
@@ -57,5 +111,7 @@ public class OpenDevShareMainActivity extends Activity {
 		};
 
 		downloadManager.execute("http://www.gemserk.com/private/prototipos/superflyingthing-latest/superflyingthing-android.apk", finalApkFile);
+
 	}
+
 }
